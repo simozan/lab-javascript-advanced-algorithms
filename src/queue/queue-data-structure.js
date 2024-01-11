554ff2e4bc93a51bd78de5cb74246d00aef8ee80
John Doe <johndoe@example.com>
2024-01-11 09:30:56 +0100

    Exercise completed

--- src/queue/queue-data-structure.js
+++ src/queue/queue-data-structure.js
@@ -5,23 +5,33 @@ class Queue {
   }
 
   canEnqueue() {
+    if (this.queueControl.length<this.MAX_SIZE){return true} else return false
     // ... your code goes here
   }
 
   isEmpty() {
+    if (this.queueControl.length===0){return true} else return false
     // ... your code goes here
   }
 
   enqueue(item) {
-    // ... your code goes here
+    if (this.canEnqueue()===true)
+    {this.queueControl.push(item)
+    return this.queueControl}
+    else throw new Error('QUEUE_OVERFLOW');
   }
 
   dequeue() {
     // ... your code goes here
+    if (this.isEmpty()===false){ const elementEliminated= this.queueControl[0]
+      this.queueControl.shift()
+      return elementEliminated
+    } else throw new Error('QUEUE_UNDERFLOW')
   }
 
   display() {
-    // ... your code goes here
+    // ... your code goes here 
+    return (this.queueControl)
   }  
 }
 
